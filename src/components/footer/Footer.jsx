2ea7efc6aacaf9ea1436db921e6155643dd27fa4
pepe <josepp2508@gmail.com>
2025-03-19 18:26:52 -0600

    [IMP] contact info in foote

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => {
     return(
         <>
             <footer className="footer-container">
+                <div className='contact-info'>
+                    <p><span>Contactanos</span></p>
+                    <p>Email: <a href="">whit3roof@proton.me</a></p>
+                    <p>Whatsapp: <a href="wa.me/7841391319">+52 784 139 1319</a></p>
+                </div>
             </footer>
         </>
     )
