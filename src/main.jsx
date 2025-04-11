395c8dfd55fb31e21fba03965476da0af9fad7c5
pepe <josepp2508@gmail.com>
2025-04-10 18:31:32 -0600

    [ADD] react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import BackgroundRemover from './pages/background-remover/BackgroundRemover.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
