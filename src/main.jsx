387c2c69f2fc0fb730897bb53d030536b761d490
pepe <josepp2508@gmail.com>
2025-04-10 18:32:44 -0600

    [FIX] null import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import BackgroundRemover from './pages/background-remover/BackgroundRemover.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
