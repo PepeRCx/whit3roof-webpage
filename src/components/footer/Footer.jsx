7d5ba65b6a888679e95a6d261deb488fa89ed514
pepe <josepp2508@gmail.com>
2025-04-10 18:29:54 -0600

    [IMP] link to mail and whatsapp

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
             <footer className="footer-container">
                 <div className='contact-info'>
                     <p><span>Contactanos</span></p>
-                    <p>Email: <a href="">whit3roof@proton.me</a></p>
-                    <p>Whatsapp: <a href="wa.me/7841391319">+52 784 139 1319</a></p>
+                    <p>Email: <a href="mailto:whit3roof@proton.me">whit3roof@proton.me</a></p>
+                    <p>Whatsapp: <a href="https://wa.me/7841391319" target="_blank">+52 784 139 1319</a></p>
                 </div>
             </footer>
         </>
